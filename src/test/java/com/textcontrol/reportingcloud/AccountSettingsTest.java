fd9de3d005c04a65a25ab5e2b6450dd40ef3f2b3
Thorsten Kummerow <thorsten@textcontrol.com>
2016-06-14 10:23:35 +0200

    Adding AccountSettings tests.

---
+++ src/test/java/com/textcontrol/reportingcloud/AccountSettingsTest.java
@@ -0,0 +1,30 @@
+package com.textcontrol.reportingcloud;
+
+import org.junit.Assert;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * The AccountSettings test class.
+ *
+ * @author Thorsten Kummerow
+ */
+public class AccountSettingsTest {
+    @Test
+    public void IsConstructedCorrectly() {
+        AccountSettings s = new AccountSettings("1234567890123", 2, 3, 100000, 500, "2016-05-24T15:24:57+00:00");
+
+        Assert.assertEquals("1234567890123", s.getSerialNumber());
+        Assert.assertEquals(2, s.getCreatedDocuments());
+        Assert.assertEquals(3, s.getUploadedTemplates());
+        Assert.assertEquals(100000, s.getMaxDocuments());
+        Assert.assertEquals(500, s.getMaxTemplates());
+        Assert.assertEquals(
+                LocalDateTime.parse("2016-05-24T15:24:57+00:00",
+                        DateTimeFormatter.ISO_OFFSET_DATE_TIME), s.getValidUntil());
+    }
+}
